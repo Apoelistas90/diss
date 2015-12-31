a8e7ad98d1d1300328c6ce8f19088caa82c1b0f5
Andys <andrew.adamides.8@gmail.com>
2015-12-31 12:52:41 +0000

    modifications to test files

--- Parser.py
+++ Parser.py
@@ -28,14 +28,15 @@ def parsefile(train_file,test_file):
 
     # Features
     teamstats = []
-    teamstatsref= ['goals','shots','fouls','corners','yellow','red']
+    totals = []
+    teamstatsref = ['goals','shots','fouls','corners','yellow','red']
     vectors=[]
     labels=[]
-    events=[]
     # goals,shots,fouls,corners,yellow,red
     for i in range(0, len(teams)):
         # teamstats.append([0.0, 0.0, 0.0, 0.0, 0.0 ,0.0])
         teamstats.append([0.0,0.0])
+        totals.append([0.0,0.0])
 
     totalmatchesperteam = []
     for i in range(0, len(teams)):
@@ -43,49 +44,35 @@ def parsefile(train_file,test_file):
 
     with open(train_file, 'r') as csvfile:
         matches = csv.reader(csvfile, delimiter=',', quotechar='|')
+        # For each match, construct an input vector
         for match in matches:
             hometeam = match[2]
             awayteam = match[3]
+            home = []
+            away = []
             for i in range(0, len(teams)):
                 if hometeam == teams[i]:
-                    computestats(match, teamstats[i],'home')
                     totalmatchesperteam[i] += 1
-                if awayteam == teams[i]:
-                    computestats(match, teamstats[i],'away')
-                    totalmatchesperteam[i] += 1
-
-
-
-
-    for i in range(0,len(teams)):
-        computeaverages(teamstats[i], totalmatchesperteam[i])
-
-
-
-    #Construct Actual Instances and aggregate at each step
-    with open(test_file, 'r') as csvfile:
-        matches = csv.reader(csvfile, delimiter=',', quotechar='|')
-        #For each match
-        k=0
-        for match in matches:
-            events.append(match)
-            hometeam = match[2]
-            awayteam = match[3]
-            home=[]
-            away=[]
-            for i in range(0,len(teams)):
-                if hometeam == teams[i]:
-                    #home = teamother[i]
+                    teamstats[i]=computestats(match, totals[i],'home',totalmatchesperteam[i])
                     home=updatestats(match, teamstats[i],'home',totalmatchesperteam[i])
-                    #print(teams[i],home,'home')
-                    totalmatchesperteam[i]+=1
+                    #if hometeam == 'Tottenham':
+                        #print home
                 if awayteam == teams[i]:
-                    #away = teamother[i]
+                    totalmatchesperteam[i] += 1
+                    if awayteam == 'Tottenham':
+                        print("1: ")
+                        print teamstats[i]
+                    teamstats[i]=computestats(match, totals[i],'away',totalmatchesperteam[i])
+                    if awayteam == 'Tottenham':
+                        print("2: ")
+                        print teamstats[i]
                     away=updatestats(match, teamstats[i],'away',totalmatchesperteam[i])
-                    #print(teams[i],away,'away')
-                    totalmatchesperteam[i]+=1
-            #print(home+away)
+                    if awayteam == 'Tottenham':
+                        print("3: ")
+                        print away
+
             vectors.append(home + away)
+
             #Get Label
             response = match[6]
             if response == 'H':
@@ -96,61 +83,63 @@ def parsefile(train_file,test_file):
                 response = 0
             labels.append(response)
 
-            #Aggregated until the last game
-
     print('Reference for team stats used: '+str(teamstatsref))
     for i in range(0,len(teams)):
         print(str(totalmatchesperteam[i])+ ' matches played'+ ' for '+teams[i]+' and final team stats are : '+str(teamstats[i]))
 
-    print(events[1])
-    print(vectors[1])
+
     vectors.pop(0)
     labels.pop(0)
+    print(vectors)
+    print(labels)
+
+
+
     #exit(0)
+
     return vectors,labels
 
 
-def computestats(partition,team,edra):
+def computestats(partition,team,edra,totalmatches):
+    stat = [0.0, 0.0]
     if edra == 'home':
-        team[0] = team[0] + int(partition[4])#goals
-        team[1] = team[1] + int(partition[12])#shots
+        team[0] += int(partition[4])
+        stat[0] = team[0]/totalmatches#goals
+        team[1] += int(partition[12])
+        stat[1] = team[1] /totalmatches#shots
         #team[2] = team[2] + int(partition[14])#fouls
         #team[2] = team[2] + int(partition[16])#corners
         #team[4] = team[4] + int(partition[18])#yellow
         #team[5] = team[5] + int(partition[20])#red
-
+        return stat
     elif edra == 'away':
-        team[0] = team[0] + int(partition[5])#goals
-        team[1] = team[1] + int(partition[13])#shots
+        team[0] += int(partition[5])
+        stat[0] = team[0]/totalmatches#goals
+        team[1] += int(partition[13])
+        stat[1] = team[1]/totalmatches#shots
         #team[2] = team[2] + int(partition[15])#fouls
         #team[2] = team[2] + int(partition[17])
         #team[4] = team[4] + int(partition[19])
         #team[5] = team[5] + int(partition[21])
-
-def computeaverages(team,total):
-    #print(team,total)
-    team[0] = team[0] / total#goals
-    team[1] = team[1] / total#shots
-    #team[2] = team[2] / total#fouls
-    #team[2] = team[2] / total
-    #team[4] = team[4] / total
-    #team[5] = team[5] / total
-    #print(team)
-
+        return stat
+# normalise
 def updatestats(partition,team,edra,totalmatches):
+    home = [0.0, 0.0]
+    away = [0.0, 0.0]
+
     if edra == 'home':
-        team[0] = ((team[0]*totalmatches) + int(partition[4]))/(totalmatches+1)#goals
-        team[1] = ((team[1]*totalmatches) + int(partition[12]))/(totalmatches+1)#shots
+        home[0] = ((team[0]*totalmatches) + int(partition[4]))/(totalmatches+1)#goals
+        home[1] = ((team[1]*totalmatches) + int(partition[12]))/(totalmatches+1)#shots
         #team[2] = ((team[2]*totalmatches) + int(partition[14]))/(totalmatches+1)#fouls
         #team[2] = ((team[2]*totalmatches) + int(partition[16]))/(totalmatches+1)
         #team[4] = ((team[4]*totalmatches) + int(partition[18]))/(totalmatches+1)
         #team[5] = ((team[5]*totalmatches) + int(partition[20]))/(totalmatches+1)
-        return team
+        return home
     elif edra == 'away':
-        team[0] = ((team[0]*totalmatches) + int(partition[5]))/(totalmatches+1)#goals
-        team[1] = ((team[1]*totalmatches) + int(partition[13]))/(totalmatches+1)#shots
+        away[0] = ((team[0]*totalmatches) + int(partition[5]))/(totalmatches+1)#goals
+        away[1] = ((team[1]*totalmatches) + int(partition[13]))/(totalmatches+1)#shots
         #team[2] = ((team[2]*totalmatches) + int(partition[15]))/(totalmatches+1)#fouls
         #team[2] = ((team[2]*totalmatches) + int(partition[17]))/(totalmatches+1)
         #team[4] = ((team[4]*totalmatches) + int(partition[19]))/(totalmatches+1)
         #team[5] = ((team[5]*totalmatches) + int(partition[21]))/(totalmatches+1)
-        return team
+        return away
